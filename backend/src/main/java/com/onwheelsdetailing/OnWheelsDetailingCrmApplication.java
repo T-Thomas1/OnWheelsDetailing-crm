963287fb2b2ec8a213d39b8aa8eb24c82ce334f1
Taso T. <hc4817@wayne.edu>
2025-08-14 19:05:42 -0400

    Update Folder structure and placeholder files. Security Configuration as well.

---
+++ backend/src/main/java/com/onwheelsdetailing/OnWheelsDetailingCrmApplication.java
@@ -0,0 +1,11 @@
+package com.onwheelsdetailing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnWheelsDetailingCrmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OnWheelsDetailingCrmApplication.class, args);
+    }
+}
